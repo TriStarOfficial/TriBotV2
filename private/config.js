4b8b391c5018faa67e5b1bf9b28e52e39ba001ef
TriStar <83173209+TriStarOfficial@users.noreply.github.com>
2021-06-08 09:18:37 +0530

    Delete private directory

--- private/config.js
+++
@@ -1,7 +0,0 @@
-module.exports.config = {
-    "token": "ODQ5NjM2NzIzMTc5OTEzMjI2.YLeD3A.ZqA__sJ9_O8VM430H-r9HELXRsk",
-    "prefix": "-",
-    "mongo": "mongodb+srv://admin:admin@tribotv2.m0jol.mongodb.net/TriBot?retryWrites=true&w=majority",
-    "TicketParent": "851450132385628210",
-    "Logs": "851450178711453716"
-}
